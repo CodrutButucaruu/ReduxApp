a394fa13e8a543634e94e03b1800b50de38d04e6
Codrut Butucaru <Codrut.Butucaru@cognizant.com>
2026-02-20 10:10:22 +0200

    initial commit

---
+++ to-do-app-redux/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddTask from './components/AddTask';
+import Tasks from './components/Tasks';
+
+const App = () => {
+    return (
+        <div>
+            <AddTask />
+            <Tasks />
+        </div>
+    );
+};
+
+export default App;
